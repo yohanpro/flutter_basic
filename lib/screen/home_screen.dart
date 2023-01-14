d64843a3509a1b85f67dcaccccf0ff2d5d070733
Yohan Kim <yohan@yohanpro.com>
2023-01-14 13:18:03 +0900

    Row and Column 강의

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -10,17 +10,78 @@ class HomeScreen extends StatelessWidget {
         bottom: false,
         child: Container(
             color: Colors.black,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.min,
               children: [
                 Container(
-                  height: 100,
-                  color: Colors.red,
-                  child: Center(
-                    child: Text(
-                      'Header',
-                      style: TextStyle(fontSize: 30, color: Colors.white),
-                    ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        width: 50.0,
+                        height: 50.0,
+                        color: Colors.red,
+                      ),
+                      Container(
+                        width: 50.0,
+                        height: 50.0,
+                        color: Colors.orange,
+                      ),
+                      Container(
+                        width: 50.0,
+                        height: 50.0,
+                        color: Colors.yellow,
+                      ),
+                      Container(
+                        width: 50.0,
+                        height: 50.0,
+                        color: Colors.green,
+                      ),
+                    ],
                   ),
+                ),
+                Container(
+                  child: Container(
+                    width: 50.0,
+                    height: 50.0,
+                    color: Colors.orange,
+                  ),
+                ),
+                Container(
+                  child: Container(
+                      child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        width: 50.0,
+                        height: 50.0,
+                        color: Colors.red,
+                      ),
+                      Container(
+                        width: 50.0,
+                        height: 50.0,
+                        color: Colors.orange,
+                      ),
+                      Container(
+                        width: 50.0,
+                        height: 50.0,
+                        color: Colors.yellow,
+                      ),
+                      Container(
+                        width: 50.0,
+                        height: 50.0,
+                        color: Colors.green,
+                      ),
+                    ],
+                  )),
+                ),
+                Container(
+                  width: 50.0,
+                  height: 50.0,
+                  color: Colors.green,
                 ),
               ],
             )),
